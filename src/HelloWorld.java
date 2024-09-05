0980af6c513f4c1e638301eb3183ff76aefe0039
Khanh Do <112164825+khanhdo05@users.noreply.github.com>
2024-09-05 15:25:42 -0500

    exercise 12

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main (String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     pen.println ("Hello, GitHub! UPDATEEEEEEE");
+    pen.println ("Hello, GitHub! UPDATEEEEEEE");
     pen.flush();
     pen.close();
   } // main(String[])
