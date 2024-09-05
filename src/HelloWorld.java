f6bd5ef6132fd47c0c7afb860b8314f4edd97610
KHANH <khanhphuongdo28@gmail.com>
2024-09-05 15:28:34 -0500

    conflict

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main (String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     pen.println ("Hello, GitHub! UPDATEEEEEEE");
-    pen.println ("Hello, GitHub! UPDATEEEEEEE");
+    pen.println ("Hello, GitHub! ");
     pen.flush();
     pen.close();
   } // main(String[])
